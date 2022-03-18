d68e71c9e5242ed909bb96a31ec622fe57afd840
alex parfenov <alexp-2004@mail.ru>
2022-03-18 20:46:01 +0700

    change some functions + add axios

--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printError, printHelp, printSuccess } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Не передан token');
+    return;
+  }
   try {
-    await saveKeyValue('token', token);
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
     printSuccess('Токен сохранён');
   } catch (e) {
     printError(e.message);
@@ -23,6 +28,7 @@ const initCLI = () => {
     saveToken(args.t);
     //Save token
   }
+  getWeather('moscow');
   //Write the weather
 };
 
