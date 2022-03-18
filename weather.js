5d5dc36afdf1503086307b4391050b532f1733a3
alex parfenov <alexp-2004@mail.ru>
2022-03-18 18:41:58 +0700

    service add

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Токен сохранён');
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
@@ -8,9 +18,9 @@ const initCLI = () => {
     printHelp();
   }
   if (args.s) {
-    //Save city
   }
   if (args.t) {
+    saveToken(args.t);
     //Save token
   }
   //Write the weather
